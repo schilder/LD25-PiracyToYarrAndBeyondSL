03649539eb3c9bd78796c0f2cceefeb75da84f13
Andrew Quesenberry <aquesenb@mavs.coloradomesa.edu>
2012-12-15 01:56:47 -0700

    First Commit

---
+++ LD25-PiracyToYarrAndBeyond/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace LD25_PiracyToYarrAndBeyond
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
